7489501cdd32f964c3cd55e23a71d156861d4622
Hugo Sousa <hmosousa@gmail.com>
2024-03-12 15:25:24 +0000

    Implemented database object.

--- src/contants.py
+++ src/contants.py
@@ -1,5 +1,16 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
 
 ROOT = Path(__file__).parent.parent
 
 DATA_PATH = ROOT / "data"
+
+load_dotenv(ROOT / ".env")
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_URL = os.getenv("MONGO_URL")
+MONGO_ENDPOINT = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_URL}"
---
+++ src/db.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+
+from src.contants import MONGO_ENDPOINT
+
+
+class Database:
+    def __init__(self, language: str = "en"):
+        self._client = MongoClient(MONGO_ENDPOINT)
+        self._db = self._client["publico"]
+        self._collection = self._db[language]
+        self._lang = language
+    
+    def insert(self, data: dict):
+        self._collection.insert_one(data)
+
+    def insert_many(self, data: list):
+        self._collection.insert_many(data)
+
+    def get(self, id: int):
+        answer = self._collection.find_one({"_id": id})
+        return answer
+
+    def delete(self, id: int):
+        self._collection.delete_one({"_id": id})
+
+    def __iter__(self):
+        return self._collection.find()
+    
+    def __len__(self):
+        return self._collection.count_documents({})
+    
+    def __del__(self):
+        self._db.drop_collection(self._lang)
+
+    def __contains__(self, id: int):
+        return bool(self.get(id))
