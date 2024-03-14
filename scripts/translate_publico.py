6b030a43fb9b106a59a16dfbfea029cc3d735c73
Hugo Sousa <hmosousa@gmail.com>
2024-03-14 15:24:10 +0000

    Added process to translate sentences with more than 5k chars.

--- scripts/translate_publico.py
+++ scripts/translate_publico.py
@@ -13,7 +13,16 @@ def main(lang="en"):
     for id_, text in tqdm(source_ds):
         if id_ not in target_ds:    
             try:
-                translate = translator.translate(text, max_chars=10_000)
+                
+                if len(text) > 5000:
+                    translate = []
+                    for sentence in text.split("\n"):
+                        translate.append(translator.translate(sentence))
+                    translate = "\n".join(translate)
+                
+                else:
+                    translate = translator.translate(text)
+                
                 target_ds.add(id_, translate)
             except Exception as e:
                 print(f"Error translating {id_}: {e}")
